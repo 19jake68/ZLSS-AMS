7a2176d264658de9b6c942feb79e725e24d605ff
Jana <Jana>
2016-06-28 22:52:29 +0800

    Added employee post and get

--- api/controllers/EmployeeController.js
+++ api/controllers/EmployeeController.js
@@ -6,6 +6,28 @@
  */
 
 module.exports = {
+	create: function(req, res) {
+		var params = req.params.all();
+		
+		Employee.create({firstname: params.firstname, lastname: params.lastname}).exec(function createCB(err, created) {
+			if (err) {
+				return res.serverError(err);
+			}
+			
+			return res.json({
+				notice: 'Created employee with name ' + created.firstname + ' ' + created.lastname
+			});
+		});
+	},
 	
+	get: function(req, res) {
+		Employee.find({}).exec(function(err, data) {
+			if (err) {
+				return res.serverError(err);
+			}
+			
+			return res.json(data);
+		});
+	}
 };
 
